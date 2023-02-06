e8599778e385f01166b815eadce7cf7ec636fe2d
ayindequdus22 <ayindeabdulqudus22@gmail>
2023-02-06 02:12:37 -0800

    made changes again

--- script/script.js
+++ script/script.js
@@ -19,4 +19,35 @@ $(document).ready(function(){
          
         
     })
+    // countdown  sect
+let daysItem = document.querySelector("#day");
+let hoursItem = document.querySelector("#hour");
+    let minItem = document.querySelector("#minute");
+    let secItem = document.querySelector("#second");
+   
+   
+    let countDown = () => {
+      let ComingDate = new Date("1 April 2023");
+      let PresentDate = new Date();
+      let myDate = ComingDate - PresentDate;
+      //console.log(myDate);
+   
+      let Futuredays = Math.floor(myDate / 1000 / 60 / 60 / 24);
+   
+      let Futurehours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+   
+      let Futuremins = Math.floor(myDate / 1000 / 60) % 60;
+   
+      let Futuresecs = Math.floor(myDate / 1000) % 60;
+   
+      daysItem.textContent = Futuredays;
+      hoursItem.textContent = Futurehours;
+      minItem.textContent = Futuremins;
+      secItem.textContent = Futuresecs;
+    }
+    countDown()   
+    setInterval(countDown, 1000)
+
+// countdown sect
+
 })
