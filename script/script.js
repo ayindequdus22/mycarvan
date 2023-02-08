c6f9f56fd1bdb3d9112e2c64e2f65d49b263b816
ayindequdus22 <ayindeabdulqudus22@gmail>
2023-02-08 22:16:30 +0100

    almost done

--- script/script.js
+++ script/script.js
@@ -1,11 +1,47 @@
 $(document).ready(function(){
+  
+    
+// filter
+$('.rangeContainer .buttons .filter-btn').click(function(){
+    const value = $(this).attr(`data-filter`);
+     $(this).addClass('active').siblings().removeClass('active');
+    if(value == 'all'){
+      $('.rangeCarContainer .rangeCar').show(10).addClass('active');
+    }
+    else{
+      $('.rangeCarContainer .rangeCar').not('.'+value).hide(10).removeClass('active');
+      $('.rangeCarContainer .rangeCar').filter('.'+value).show(10).addClass('active');
+     }
    
+    })
+// filter
+$('.brandContainer .buttons .filter-btn').click(function(){
+    const value = $(this).attr(`data-filter`);
+     $(this).addClass('active').siblings().removeClass('active');
+    if(value == 'all'){
+      $('.brandCarContainer .brandCar').show(10).addClass('active');
+    }
+    else{
+      $('.brandCarContainer .brandCar').not('.'+value).hide(10).removeClass('active');
+      $('.brandCarContainer .brandCar').filter('.'+value).show(10).addClass('active');
+     }
+   
+    })
+
+
+
+
    $('.nav .icons .fa-bars').click(function(){
     $(this).toggleClass("fa-times ")
     $('nav .LinkContainer').toggleClass('active')
    })
    
-   
+   $('.UpBtn').click(function(){
+    window.scrollTo({top:0,left:0,behavior:'smooth'})
+     });
+     $('.DownBtn').click(function(){
+        window.scrollTo({top:document.documentElement.scrollHeight,behavior:'smooth'})
+     })
     $(window).scroll(function(){
         if($(window).scrollTop() > 100){
             $('.nav').addClass('active')
@@ -13,6 +49,13 @@ $(document).ready(function(){
     else{
  $('.nav').removeClass('active')
     }
+    if(scrollY > 50){
+        $('.topCar div').addClass('active');
+        }
+         else{
+            $('.topCar div').removeClass('active');
+             };
+              
     $('nav .LinkContainer').removeClass('active')
     $('.nav .icons .fa-bars').removeClass("fa-times ")
     
@@ -27,7 +70,7 @@ let hoursItem = document.querySelector("#hour");
    
    
     let countDown = () => {
-      let ComingDate = new Date("1 April 2023");
+      let ComingDate = new Date("1 May 2023");
       let PresentDate = new Date();
       let myDate = ComingDate - PresentDate;
       //console.log(myDate);
